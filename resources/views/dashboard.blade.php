98df98ddc82ca13a7cea3137d690cb780fa737b3
saputra_tolol <kimocijr35@gmail.com>
2024-03-19 21:25:51 +0700

    saputra
    membuat table absensi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,62 +3,158 @@
 
 @endpush
 @section('content')
+    <!-- Hoverable Table rows -->
+    <div class="card">
+        <h5 class="card-header">rows</h5>
+        <div class="table-responsive text-nowrap">
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th rowspan="2">#</th>
+                        <th rowspan="2">Profil</th>
+                        <th rowspan="2">Nama</th>
+                        <th rowspan="2">L/P</th>
+                        <th colspan="31" id="tanggalHeader">tanggal</th>
+                        <th colspan="4">jumlah</th>
 
-  <!-- Hoverable Table rows -->
-  <div class="card">
-    <h5 class="card-header">Hoverable rows</h5>
-    <div class="table-responsive text-nowrap">
-      <table class="table table-hover">
-        <thead>
-          <tr>
-            <th>Profil</th>
-            <th>Nama</th>
-            <th>Jenis Kelamin</th>
-            <th>No.Telefone</th>
-            <th>Keterangan</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody class="table-border-bottom-0">
-            <tr>
-             <td>
-                    <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar" style="width: 50px" class="rounded-circle">
-            </td>
-            <td>adi bagus saputra</td>
-            <td>Laki-laki</td>
-            <td>085345262</td>
-            <td><span class="badge bg-label-primary me-1">hadir</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-            <tr>
-             <td>
-                    <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar" style="width: 50px" class="rounded-circle">
-            </td>
-            <td>adi bagus</td>
-            <td>Laki-laki</td>
-            <td>085345262</td>
-            <td><span class="badge bg-label-danger me-1">alpha</span></td>
-            <td>
-              <div class="dropdown">
-                <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ti ti-dots-vertical"></i></button>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-pencil me-1"></i> Edit</a>
-                  <a class="dropdown-item" href="javascript:void(0);"><i class="ti ti-trash me-1"></i> Delete</a>
-                </div>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                    </tr>
+                    <tr id="tableheader">
+                    </tr>
+                </thead>
+                <script>
+                    // Dapatkan tanggal sekarang
+                    const now = new Date();
+
+                    // Dapatkan jumlah hari dalam bulan ini
+                    const totalDaysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+
+                      // Ubah nilai atribut colspan pada elemen tanggal
+                      const tanggalHeader = document.getElementById('tanggalHeader');
+                    tanggalHeader.colSpan = totalDaysInMonth;
+
+                    // Ambil elemen tabel header
+                    const tableHeader = document.getElementById('tableheader');
+
+                    // Tambahkan elemen-elemen th ke tabel header berdasarkan jumlah hari dalam bulan
+                    for (let i = 1; i <= totalDaysInMonth; i++) {
+                        const th = document.createElement('th');
+                        th.textContent = i;
+                        tableHeader.appendChild(th);
+                    }
+
+                     // Tambahkan elemen untuk H, T, S, A setelah elemen tanggal
+                    const thH = document.createElement('th');
+                    thH.textContent = 'H';
+                    tableHeader.appendChild(thH);
+
+                    const thT = document.createElement('th');
+                    thT.textContent = 'T';
+                    tableHeader.appendChild(thT);
+
+                    const thS = document.createElement('th');
+                    thS.textContent = 'S';
+                    tableHeader.appendChild(thS);
+
+                    const thA = document.createElement('th');
+                    thA.textContent = 'A';
+                    tableHeader.appendChild(thA);
+                </script>
+
+                <tbody class="table-border-bottom-0">
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar" style="width: 50px"
+                                class="rounded-circle">
+                        </td>
+                        <td>adi bagus saputra</td>
+                        <td>L</td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td><span class="badge bg-label-primary me-1">H</span></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                        <td>6</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>
+                            <img src="{{ asset('assets/img/avatars/5.png') }}" alt="Avatar" style="width: 50px"
+                                class="rounded-circle">
+                        </td>
+                        <td>adi bagus</td>
+                        <td>L</td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td><span class="badge bg-label-danger me-1">A</span></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>6</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
+<<<<<<< Updated upstream
   </div>
   <!--/ Hoverable Table rows -->
 
@@ -66,3 +162,8 @@
  @endsection
 
 
+=======
+    <!--/ Hoverable Table rows -->
+    <hr>
+@endsection
+>>>>>>> Stashed changes
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,10 +16,7 @@
     <meta name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Tables - Basic Tables |
-        Vuexy -
-        Bootstrap Admin Template
-    </title>
+    <title>Absensi Siswa</title>
     <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
     <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
     <!-- laravel CRUD token -->
